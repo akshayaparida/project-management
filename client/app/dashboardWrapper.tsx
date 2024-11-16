3a512053a314393050829f69668bea5b6a1f4dc2
Akshaya Parida <akshayaparida2811@gmail.com>
2024-11-16 22:05:28 +0530

    added search bar and settings icon in Navbar

---
+++ client/app/dashboardWrapper.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from "@/app/(components)/Navbar";
+
+
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/*sidebar */}
+
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg `}   >
+         {/*navbar*/}
+
+         <Navbar/>
+         {children}
+      </main>
+</div>
+  );
+};
+export default DashboardWrapper
